4c264bc5b520ed28669b083af782c7b236ddc87b
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-03-08 17:31:27 +0545

    Created TaskModel and TodoModel

---
+++ lib/Models/TodoModel.dart
@@ -0,0 +1,8 @@
+import 'package:to_do_app/Models/TaskModel.dart';
+import 'package:flutter/cupertino.dart';
+
+class TodoModel {
+  List<TaskModel> taskList = []; //Contains all the task
+
+  addTaskInList() {}
+}
