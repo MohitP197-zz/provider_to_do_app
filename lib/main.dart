6d7b368c39db256c24fd7a6636744bf13801af40
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-03-08 12:57:54 +0545

    Added boxdecoration

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class MyHomePage extends StatelessWidget {
         elevation: 0,
         title: Text(
           "Todo Application",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
         leading: IconButton(
           icon: Icon(
@@ -46,9 +48,10 @@ class MyHomePage extends StatelessWidget {
               Text(
                 "02 : 36 PM",
                 style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 45,
-                    fontWeight: FontWeight.bold),
+                  color: Colors.white70,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Text(
                 "current time",
@@ -64,7 +67,49 @@ class MyHomePage extends StatelessWidget {
           ),
           Expanded(
             child: Container(
-              decoration: BoxDecoration(),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  topLeft: Radius.circular(60),
+                ),
+                color: Colors.white,
+              ),
+              child: ListView.builder(
+                  itemCount: 1,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: ListTile(
+                        contentPadding: EdgeInsets.only(
+                          left: 32,
+                          top: 8,
+                          bottom: 8,
+                        ),
+                        title: Text(
+                          "Build new app",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Build a new app on flutter',
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        trailing: Icon(
+                          Icons.check_circle,
+                          color: Colors.greenAccent,
+                        ),
+                      ),
+                      margin: EdgeInsets.only(
+                        bottom: 8,
+                        left: 16,
+                        right: 40,
+                      ),
+                    );
+                  }),
             ),
           )
         ],
